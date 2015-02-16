58944c83856383eb6eab7b8101a3f70b9f4f6ffc
shiznewski <stnewell@gmail.com>
2015-02-16 15:22:29 -0500

    asdf
    asdf

--- www/files/stn.js
+++ www/files/stn.js
@@ -394,31 +394,50 @@ function checkLogin(type){
 
 //Cookie Handling
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+	if (isPhonegap()){
+		//write local stoare
+		window.localStorage.setItem(cname, cvalue);
+	}
+	else{
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
 } 
 
 
-function checkCookie() {
+function checkCookie(redir) {
     var username=getCookie("session");
     if (username!="") {
         //alert("Welcome again " + username);
-		window.location.href = "manage_bar.html";
+		if (redir == true)
+			window.location.href = "manage_bar.html";
+		else{
+			$("#logindiv").html('Set: ' + username);
+		}
     }else{
 		//alert('Not logged in');
+		$("#logindiv").html('None:');
     }
 } 
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
+
+	if(isPhonegap()){
+		//get local storage
+		var value = window.localStorage.getItem(cname);
+		return value;
+	}
+	else{
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+	}
+	return "";
 } 
 //End cookie handling
